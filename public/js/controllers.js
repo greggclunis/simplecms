b629ccb9ea91b007c9d50bb1261340016e09c0ff
Gregg Clunis <Gregg Clunis>
2017-03-14 20:59:28 -0400

    First commit

---
+++ public/js/controllers.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/* Controllers */
+  
+angular.module('myApp.controllers', []).
+controller('AdminPagesCtrl', ['$scope', '$log', 'pagesFactory',
+  function($scope, $log, pagesFactory) {
+    pagesFactory.getPages().then(
+      function(response) {
+        $scope.allPages = response.data;
+      },
+      function(err) {
+        $log.error(err);
+      });
+
+      $scope.deletePage = function(id) {
+        pagesFactory.deletePage(id);
+      };
+
+    }
+]);
+
+angular.module('myApp.controllers', []).
+controller('AdminLoginCtrl', ['$scope', '$location', '$cookies', 'AuthService','$log',
+      function($scope, $location, $cookies, AuthService, $log) {
+        $scope.credentials = {
+          username: '',
+          password: ''
+        };
+        $scope.login = function(credentials) {
+          AuthService.login(credentials).then(
+            function(res, err) {
+              $cookies.loggedInUser = res.data;
+              $location.path('/admin/pages');
+            },
+            function(err) {
+              $log.log(err);
+            });
+          };
+      }
+]);
